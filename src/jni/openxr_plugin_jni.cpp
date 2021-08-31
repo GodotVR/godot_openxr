321c2296b26591cc408ae959e141fe29fb260407
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2021-08-31 10:30:33 -0700

    Add stub classes/code to make the functionality available to the java layer.

---
+++ src/jni/openxr_plugin_jni.cpp
@@ -0,0 +1,15 @@
+/**
+* Created by Fredia Huya-Kouadio. 
+*/
+
+#include <jni.h>
+
+#undef JNI_PACKAGE_NAME
+#define JNI_PACKAGE_NAME org_godotengine_plugin_vr_openxr
+
+#undef JNI_CLASS_NAME
+#define JNI_CLASS_NAME OpenXRPlugin
+
+extern "C" {
+
+}

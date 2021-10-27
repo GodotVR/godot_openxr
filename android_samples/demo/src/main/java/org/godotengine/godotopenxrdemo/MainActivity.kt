56dd96b89b2ee5286e14fbe9b576ae69ac9bf5bb
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2021-10-27 12:04:57 -0700

    Add support for a stub version of the Android library without the native dependencies
    Given the Android native shared libraries are only available for arm architectures, this enables to simulate the use of the Android library on x86 architectures.

--- android_samples/demo/src/main/java/org/godotengine/godotopenxrdemo/MainActivity.kt
+++ android_samples/demo/src/main/java/org/godotengine/godotopenxrdemo/MainActivity.kt
@@ -2,11 +2,16 @@ package org.godotengine.godotopenxrdemo
 
 import org.godotengine.godot.FullScreenGodotApp
 import org.godotengine.godot.xr.XRMode
+import java.util.*
 
 class MainActivity : FullScreenGodotApp() {
 
     // Override the engine starting parameters to indicate we want VR mode.
     override fun getCommandLine(): List<String> {
-        return listOf(XRMode.OVR.cmdLineArg)
+        return if (BuildConfig.FLAVOR == "ovr") {
+            listOf(XRMode.OVR.cmdLineArg)
+        } else {
+            Collections.emptyList()
+        }
     }
 }

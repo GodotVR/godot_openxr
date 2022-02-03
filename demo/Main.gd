2e6652857da81e3f6668ddf9a4a5647dd61371a2
Bastiaan Olij <mux213@gmail.com>
2022-02-03 23:13:52 +1100

    Renaming FPSController to FPController

--- demo/Main.gd
+++ demo/Main.gd
@@ -11,26 +11,26 @@ func _process(delta):
 	# We minipulate our origin point to move around. Note that with roomscale tracking a little more then this is needed
 	# because we'll rotate around our origin point, not around our player. But that is a subject for another day.
 	if (Input.is_key_pressed(KEY_LEFT)):
-		$FPSController.rotation.y += delta
+		$FPController.rotation.y += delta
 	elif (Input.is_key_pressed(KEY_RIGHT)):
-		$FPSController.rotation.y -= delta
+		$FPController.rotation.y -= delta
 
 	if (Input.is_key_pressed(KEY_UP)):
-		$FPSController.translation -= $FPSController.transform.basis.z * delta;
+		$FPController.translation -= $FPController.transform.basis.z * delta;
 	elif (Input.is_key_pressed(KEY_DOWN)):
-		$FPSController.translation += $FPSController.transform.basis.z * delta;
+		$FPController.translation += $FPController.transform.basis.z * delta;
 
 	# this is a little dirty but we're going to just tie the trigger input of our controllers to their haptic output for testing
-	$FPSController/LeftHandController.rumble = $FPSController/LeftHandController.get_joystick_axis(JOY_VR_ANALOG_TRIGGER)
-	$FPSController/RightHandController.rumble = $FPSController/RightHandController.get_joystick_axis(JOY_VR_ANALOG_TRIGGER)
+	$FPController/LeftHandController.rumble = $FPController/LeftHandController.get_joystick_axis(JOY_VR_ANALOG_TRIGGER)
+	$FPController/RightHandController.rumble = $FPController/RightHandController.get_joystick_axis(JOY_VR_ANALOG_TRIGGER)
 
-func _on_FPSController_initialised():
+func _on_FPController_initialised():
 	# Just for testing, output the enabled extensions
-	print("Enabled extensions: " + str($FPSController/Configuration.get_enabled_extensions()))
-	print("Supported refresh rates: " + str($FPSController/Configuration.get_available_refresh_rates()))
-	print("Supported color spaces: " + str($FPSController/Configuration.get_available_color_spaces()))
-	print("Current color space: " + str($FPSController/Configuration.get_color_space()))
+	print("Enabled extensions: " + str($FPController/Configuration.get_enabled_extensions()))
+	print("Supported refresh rates: " + str($FPController/Configuration.get_available_refresh_rates()))
+	print("Supported color spaces: " + str($FPController/Configuration.get_available_color_spaces()))
+	print("Current color space: " + str($FPController/Configuration.get_color_space()))
 
-func _on_FPSController_failed_initialisation():
+func _on_FPController_failed_initialisation():
 	# exit our app
 	get_tree().quit()
